0bb68245cede293bdac482ae9b45878ac96c63b2
Zhi Peng <FZHIPENG09.25@ICHAT.SP.EDU.SG>
2026-05-22 22:43:04 +0800

    tested

--- test_Lab3.py
+++ test_Lab3.py
@@ -28,3 +28,22 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_with_10_or_more_element():
+    result = []
+    input_arr = [1,2,3,4,5,6,7,8,9,10,11]
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert(result == 1)
+
+
+def test_bubble_sort_empty_array():
+    result = []
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert(result == 0)
+
+def test_bubble_sort_non_digit_array():
+    result = []
+    input_arr = [1,1,1,1,"hello",1,1,1,1]
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert(result == 2)
